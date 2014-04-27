26c58e511794e2a49e6803b9ed07b2f13fff2e55
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-04-27 08:58:29 +0700

    construct the build process (2)

---
+++ ws/cpp/src/pre/ecg_signal_preprocessor.cc
@@ -0,0 +1,13 @@
+#include "ecg_signal_preprocessor.h"
+  
+using namespace lab1231_ecg_prj;
+
+void ECGSignalPreprocessor::preprocess() {
+  using namespace std;
+  
+  cout << "Preprocessing: BEGIN\n";
+  //...
+  cout << "Preprocessing: END\n";
+}
+
+  
