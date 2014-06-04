577d194f7acea6e86bd81e7c3c8296df6da26c49
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-06-04 11:51:13 +0700

    print cv-data, fix preprocessing

--- ws/python/Lab1231ECGPrj/test/fsvm-cil/eval.py
+++ ws/python/Lab1231ECGPrj/test/fsvm-cil/eval.py
@@ -7,15 +7,16 @@ import csv
 
 import util
 
+dir_path = 'out/perf-double-preprocessing/'
 dbs = None
-with open('out/dataset_names.csv', 'rb') as csvfile:
+with open(dir_path + 'dataset_names.csv', 'rb') as csvfile:
     csv_reader = csv.reader(csvfile)
     for row in csv_reader:
         dbs= row
 
 # 
-mean_perfs = np.genfromtxt('out/normalSVM_mean_perfs.csv', dtype=float, delimiter=',') 
-std_perfs = np.genfromtxt('out/normalSVM_std_perfs.csv', dtype=float, delimiter=',') 
+mean_perfs = np.genfromtxt(dir_path + 'normalSVM_mean_perfs.csv', dtype=float, delimiter=',') 
+std_perfs = np.genfromtxt(dir_path + 'normalSVM_std_perfs.csv', dtype=float, delimiter=',') 
 
 # 
 util.plot_bar(mean_perfs[:,0], std_perfs[:,0], 'Gmean', dbs, 'Normal SVM')
--- ws/python/Lab1231ECGPrj/test/fsvm-cil/pipeline.py
+++ ws/python/Lab1231ECGPrj/test/fsvm-cil/pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -8,6 +9,7 @@ import normalSVM
 
 # Load
 dataset_names = ['glass', 'pima-indians-diabetes', 'haberman', 'page-blocks', 'abalone', 'yeast', 'transfusion']
+#dataset_names = ['abalone', 'yeast']
 datasets = [loader.load(e) for e in dataset_names]
 
 # Init perf, column => gmean, SE, SP
@@ -20,10 +22,15 @@ for i, dataset in enumerate(datasets):
     
     X, y = dataset
     mean_perfs[i,:], std_perfs[i,:] = normalSVM.run(X, y)
-
+    
 # Save perf
-np.savetxt("out/normalSVM_mean_perfs.csv", mean_perfs, delimiter=",")
-np.savetxt("out/normalSVM_std_perfs.csv", std_perfs, delimiter=",")
-with open('out/dataset_names.csv', 'wb') as csvfile:
+dir_path = 'out/perf-double-preprocess/'
+
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
+            
+np.savetxt(dir_path+"normalSVM_mean_perfs.csv", mean_perfs, delimiter=",")
+np.savetxt(dir_path+"normalSVM_std_perfs.csv", std_perfs, delimiter=",")
+with open(dir_path+'dataset_names.csv', 'wb') as csvfile:
     csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerow(dataset_names)
