020cac553bb04bc19465f066cd00acd3c7ac6d86
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-06-11 19:36:26 +0700

    add mat2scalar()

--- ws/cpp/src/util/eigenlib_support.h
+++ ws/cpp/src/util/eigenlib_support.h
@@ -10,6 +10,7 @@ class EigenLibSupport {
   static Eigen::MatrixXd remove_rows(const Eigen::MatrixXd& M, const uint64_t& pstart, const uint64_t& pend);
   static Eigen::MatrixXd remove_row(const Eigen::MatrixXd& M, const uint64_t& ith);
   static Eigen::MatrixXd scalar2mat(const double& scalar);
+  static double mat2scalar(const Eigen::MatrixXd& mat);
 };
 
 }// namespace lab1231_ecg_prj
